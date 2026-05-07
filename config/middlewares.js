88d1549f63f9783c24ea388cb1cc3cdd4630f5d1
Gianluca Baldoni <g.baldoni@drop.it>
2026-05-07 10:05:34 +0200

    feat: linode plugin for cdn strapi

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,34 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io', // Necessario per l'admin di Strapi
+            '*.linodeobjects.com',      // Permette le immagini da Linode
+            'apparelassets.premiata.it'
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            '*.linodeobjects.com',      // Permette i video/audio da Linode
+            'apparelassets.premiata.it'
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
