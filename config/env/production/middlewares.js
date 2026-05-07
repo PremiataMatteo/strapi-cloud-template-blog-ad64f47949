412e2628d04abf841ef673b88b4ff339a140d609
Gianluca Baldoni <g.baldoni@drop.it>
2026-05-07 17:16:36 +0200

    fix: test middlewares

---
+++ config/env/production/middlewares.js
@@ -0,0 +1,43 @@
+module.exports = () => [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            '*.linodeobjects.com',
+            'apparelassets.premiata.it',
+            '*.premiata.it',
+            '*.strapiapp.com'
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            '*.linodeobjects.com',
+            'apparelassets.premiata.it',
+            '*.premiata.it',
+            '*.strapiapp.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
